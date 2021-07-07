8718306caad7f859314b044c21029ce7f890d498
Chris Mac <hyer.chris@gmail.com>
2021-07-07 12:22:33 -0700

    completely scrapped old js. it was a mess. add new, clear, addBookToLibrary function

--- script.js
+++ script.js
@@ -10,43 +10,19 @@ function Book(title, author, pages, readStatus) {
 
 Book.prototype.toString = () => console.log(this.title, this.author);
 
+// function goal: get user input, turn it into a book object, 
+// add that book object to the myLibrary array
+
 function addBookToLibrary() {
-    
-    myLibrary.push(new Book(document.getElementById('inputTitle').value,document.getElementById('inputAuthor').value,document.getElementById('inputPages').value, 'Unread'));
-    let bookshelf = document.getElementById('bookshelf');
-    let bookEle = document.createElement('div');
+    let newTitle = document.getElementById('inputTitle').value;
+    let newAuthor = document.getElementById('inputAuthor').value;
+    let newPages = document.getElementById('inputPages').value;
+    let newStatus = document.querySelector('input[name=status]:checked').value;
 
-    let buttonDiv = document.createElement('div');
-    buttonDiv.setAttribute('class', 'bookButtons');
-    
-    let deleteButton = document.createElement('button');
-    deleteButton.setAttribute('id','deleteButton');
-    deleteButton.textContent = 'Delete';
+    let newBook = new Book(newTitle,newAuthor,newPages,newStatus);
 
-    let editButton = document.createElement('button');
-    editButton.setAttribute('id','editButton');
-    editButton.textContent = 'Edit';
-
-    buttonDiv.appendChild(deleteButton);
-    buttonDiv.appendChild(editButton);
-
-    let titleEle = document.createElement('h3');
-    titleEle.textContent = book.title;
-    let authEle = document.createElement('h5');
-    authEle.textContent = book.author;
-    let pageEle = document.createElement('p');
-    pageEle.textContent = book.pages;
-    let readEle = document.createElement('p');
-    readEle.textContent = book.readStatus;
-    bookEle.setAttribute('class','book');
-    
-    bookEle.appendChild(buttonDiv);
-    bookEle.appendChild(titleEle);
-    bookEle.appendChild(authEle);
-    bookEle.appendChild(pageEle);
-    bookEle.appendChild(readEle);
-    bookshelf.appendChild(bookEle);
-    console.log(book.title);
+    myLibrary.push(newBook);
+    console.log(myLibrary);
 
 }
 document.getElementById('addBook').addEventListener('click',addBookToLibrary);
@@ -56,5 +32,5 @@ function removeBookFromLibrary(bookToBeRemoved) {
 }
 
 function displayLibrary() {
-    
+
 }
